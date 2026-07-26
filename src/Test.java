bde440e4e9d58ba8d8fcc2037ccecd4d571d03dd
aparna <aparnakolagani1919@gmail.com>
2026-07-26 21:13:05 +0530

    added new feature

--- src/Test.java
+++ src/Test.java
@@ -11,4 +11,17 @@ public class Test {
     public static int mul(int a, int b) {
         return a+b;
     }
+
+
+         System.out.println(add(12, 67));
+        System.out.println(mul(2, 7));
+
+        System.out.println(isEven(10));
+        System.out.println(isEven(15));
+        System.out.println(isEven(100));
+
+public static boolean isEven(int num) {
+    return num % 2 == 0;
+}
+}
 }
