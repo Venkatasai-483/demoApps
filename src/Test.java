2f0e58a7c05ae1883d92665ecf2a2d3bb992d1f8
Venkatasai-483 <sailagisetty995@gmail.com>
2026-07-26 10:14:24 +0530

    added the multiplication method in Test class

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(add(12, 67));
+        System.out.println(mul(2, 7));
+    }
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static int mul(int a, int b) {
+        return a+b;
+    }
+}
