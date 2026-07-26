0b425fa8460ff6c4bca0bbe8f303f1003babca79
aparna <aparnakolagani1919@gmail.com>
2026-07-26 17:49:50 +0530

    added new feature branch

---
+++ src/FeatureBranch.java
@@ -0,0 +1,23 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(add(12, 67));
+        System.out.println(mul(2, 7));
+
+        System.out.println(isEven(10));
+        System.out.println(isEven(15));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+}
