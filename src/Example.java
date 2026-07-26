5a47cf08bc304906fc9961e780a789ba19f870fd
Venkatasai-483 <sailagisetty995@gmail.com>
2026-07-26 09:56:28 +0530

    Added java files

---
+++ src/Example.java
@@ -0,0 +1,9 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(sub(89, 57));
+    }
+
+    public static int sub(int a, int b) {
+        return a-b;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(add(12, 67));
+    }
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+}
