8129fcffbab2d32df800d0011260f98057e4be8c
reddaiah-2130 <reddymallisetty09@gmail.com>
2026-07-27 11:45:02 +0530

    Created Division Program

---
+++ src/Division.java
@@ -0,0 +1,13 @@
+public class DivisionExample {
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 5;
+
+        int result = num1 / num2;
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Division Result: " + result);
+    }
+}
