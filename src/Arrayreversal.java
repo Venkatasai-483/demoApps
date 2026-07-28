911e4e02e718de299a542c585682286e3bfd5f30
dineshKotla11 <reddydineshstudent@gmail.com>
2026-07-28 18:32:48 +0530

    Added a feature to reverse the array

--- src/Arrayreversal.java
+++ src/Arrayreversal.java
@@ -1,7 +1,7 @@
 import java.utils.Arrays;
 public class Arrayreversal {
     public static void main(String[] args) {
-        int[] numbers = {10, 20, 30, 40, 50, 60};
+        int[] numbers = {10, 20, 30, 40, 50, 60,22,44,13,57};
         System.out.println("Original Array: "+Arrays.toString(numbers));
         reverse(numbers);
         System.out.println("Reversed Array"+Arrays.toString(numbers));
@@ -9,6 +9,7 @@ public class Arrayreversal {
 
     public static void reverse(int[] arr) {
         int left = 0, right = arr.length - 1;
+        //if we do know the length of array we go for "for loop" or else go for "while loop"
         while (left < right) {
             int temp = arr[left];
             arr[left] = arr[right];
